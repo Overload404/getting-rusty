2b35bbed2547d314982bfbd55bd5bf345c2ffcca
Overload404 <myhulu@ex.ua>
2023-10-18 15:46:46 +0300

    matchflow + reverse-string

--- projects/match_flow/src/main.rs
+++ projects/match_flow/src/main.rs
@@ -14,6 +14,12 @@ fn value_in_cents(coin: Coin) -> u8 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
 
 fn main() {
     println!("{}", value_in_cents(Coin::Quarter));
@@ -21,10 +27,4 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
-    fn plus_one(x: Option<i32>) -> Option<i32> {
-        match x {
-            None => None,
-            Some(i) => Some(i + 1),
-        }
-    }
 }
